f2f20ad72cf0ff645acd973656dec67121abbc4e
Shubham Vaidya <shubham@p3clap-f54mzc2.dir.svc.accenture.com>
2024-12-05 12:07:20 -0600

    Github workflow for python components

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+from src.calculator.operations import Calculator
+
+__version__ = "1.0.0"
---
+++ tests/test_operations.py
@@ -0,0 +1,26 @@
+# tests/test_operations.py
+from src.calculator.operations import Calculator
+
+import pytest
+
+
+def test_add():
+    assert Calculator.add(2, 3) == 5
+    assert Calculator.add(-1, 1) == 0
+
+
+def test_subtract():
+    assert Calculator.subtract(5, 3) == 2
+    assert Calculator.subtract(1, 1) == 0
+
+
+def test_multiply():
+    assert Calculator.multiply(2, 3) == 6
+    assert Calculator.multiply(-2, 3) == -6
+
+
+def test_divide():
+    assert Calculator.divide(6, 2) == 3
+    assert Calculator.divide(5, 2) == 2.5
+    with pytest.raises(ValueError):
+        Calculator.divide(5, 0)
